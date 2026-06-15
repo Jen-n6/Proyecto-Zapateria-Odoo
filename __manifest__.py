a023641a02a95c15d17e45be976ae6bd1d780508
Jen-n6 <jennifer.quizhpi@ute.edu.ec>
2026-06-15 17:47:35 -0500

    Agregar manifest e init

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Zapatos Extension',
+    'version': '1.0',
+    'summary': 'Extension del modulo zapatos',
+    'author': 'Jennifer',
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'depends': ['zapatos'],
+    'data': [
+        'views/zapato_extension_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
